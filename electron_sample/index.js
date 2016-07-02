9024b485d1298f82eac2fbfa50538d2485938a7d
chanu <yuyutyanu@yahoo.co.jp>
2016-07-02 15:55:21 +0900

    test

--- electron_sample/index.js
+++ electron_sample/index.js
@@ -6,22 +6,19 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 
-// �S�ẴE�B���h�E���������I��
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-// Electron�̏������������Ɏ��s
+
 app.on('ready', function() {
-    // ���C�����ʂ̕\���B�E�B���h�E�̕��A�������w���ł���
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600
     });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-    // �E�B���h�E�������ꂽ���A�v�����I��
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
